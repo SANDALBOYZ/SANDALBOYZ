be532a1855971768f05fe066edd6e051122957f4
Andrew Coelho <acoelho.baa@gmail.com>
2019-10-17 12:30:08 -0700

    Connect newsletter form

--- src/components/Subscribe/index.js
+++ src/components/Subscribe/index.js
@@ -6,12 +6,30 @@ import Input from '@components/formElements/Input';
 import * as styled from './styles';
 
 class Subscribe extends Component {
+  state = {
+    email: '',
+  };
+
+  handleChange = evt => {
+    this.setState({ email: evt.target.value });
+  };
+
   render() {
     return (
       <styled.Wrapper>
         <H200>Subscribe to our mailing list.</H200>
-        <styled.Form>
-          <Input name="email" size="large" type="email" placeholder="email@example.com" />
+        <styled.Form
+          action="https://sandalboyz.us13.list-manage.com/subscribe/post?u=3f88b88a15f3d33d800b219a9&id=e87abd7b3e"
+          method="post"
+        >
+          <Input
+            name="EMAIL"
+            size="large"
+            type="email"
+            placeholder="email@example.com"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
           <Button>Subscribe</Button>
         </styled.Form>
       </styled.Wrapper>
