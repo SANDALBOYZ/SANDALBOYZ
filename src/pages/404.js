e0ac539d8472daf71a3b2280c8c6e5c1e3aeb94f
Andrew Coelho <acoelho.baa@gmail.com>
2019-10-23 13:28:52 -0700

    Add 404 page styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Head from '@utils/seo';
+import space from '@utils/space';
+import { Body, H100 } from '@utils/type';
+import BaseButton from '@components/Button';
+
+const Button = styled(BaseButton)`
+  margin-top: ${space[4]};
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 80px;
+  padding: ${space[8]} 0;
+`;
+
+const Heading = styled(H100)`
+  margin-bottom: ${space[2]};
+`;
 
 const NotFoundPage = () => (
   <>
-    <Head title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Head title="404 Not found" />
+    <Container>
+      <Heading>404</Heading>
+      <Body>Sorry, we couldn't find what you're looking for.</Body>
+      <Button href="/" size="small">
+        Return to home
+      </Button>
+    </Container>
   </>
 );
 
