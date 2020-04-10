1f517ae4bde7a1adfeea05ce3dbd8e76e7aba40a
Ryan Chang <ry4nch4ng@gmail.com>
2020-04-10 14:48:26 -0700

    Feat/zendesk lighthouse opt (#58)
    * adds async to onrenderbody
    
    * postbody
    
    * body
    
    * edits

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,8 @@
 const React = require('react');
 
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-});
+// require('dotenv').config({
+//   path: `.env.${process.env.NODE_ENV}`,
+// });
 
 const zESettings = {
   webWidget: {
@@ -11,11 +11,9 @@ const zESettings = {
       title: {
         '*': 'Help',
       },
-      subject: true,
       fields: [
-        { id: 'subject', prefill: { '*': 'Order Number (if applicable)' } },
         { id: 'description', prefill: { '*': 'My dog ate my sandal!' } },
-        { id: 360038322313, prefill: { '*': 'ABC123' } },
+        { id: 360038322313, prefill: { '*': '' } },
       ],
     },
   },
@@ -25,8 +23,8 @@ const contactFormSettings = `
   window.zESettings = ${JSON.stringify(zESettings)};
 `;
 
-exports.onRenderBody = ({ pathname, setHeadComponents }) => {
-  return setHeadComponents([
+exports.onRenderBody = ({ pathname, setPostBodyComponents }) => {
+  return setPostBodyComponents([
     <script
       type="text/javascript"
       id="ze-snippet"
