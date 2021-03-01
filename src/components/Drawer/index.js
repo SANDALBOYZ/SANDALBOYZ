63fa2e203d92446c6711f698e95989acd76d6666
Ryan Chang <rchang@videoamp.com>
2021-03-01 10:46:47 -0800

    bettern naming

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import get from 'lodash/get';
 
 import Button from '@components/Button';
-import X from '@images/X.svg';
+import xIcon from '@images/x.svg';
 import * as styled from './styles';
 
 export function Drawer({ actions, children, onClose, open, title, loading, backgroundColor }) {
@@ -26,11 +26,11 @@ export function Drawer({ actions, children, onClose, open, title, loading, backg
   });
 
   return (
-    <styled.DrawerWrapper open={open} onClick={onClose} cursorIcon={X}>
+    <styled.DrawerWrapper open={open} onClick={onClose} cursorIcon={xIcon}>
       <styled.Drawer open={open} onClick={handleClick}>
         {loading && <styled.LoadingBar />}
-          {title && <styled.H3>{title}</styled.H3>}
-          {children}
+        {title && <styled.H3>{title}</styled.H3>}
+        {children}
         {actions && (
           <styled.Actions>
             <Button
