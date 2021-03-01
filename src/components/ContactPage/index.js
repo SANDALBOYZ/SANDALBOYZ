ddfea6d6c68a84e0fb489db7632fc55456df6052
Ryan Chang <rchang@videoamp.com>
2021-02-28 19:57:43 -0800

    cleanup contact page

--- src/components/ContactPage/index.js
+++ src/components/ContactPage/index.js
@@ -1,81 +1,83 @@
-import React, { useState } from 'react';
+import React from 'react';
+import styled from 'styled-components';
 
-import { Body } from '@utils/type';
-// import Button from '@components/Button';
-// import Input from '@components/formElements/Input';
-// import Textarea from '@components/formElements/Textarea';
-import * as styled from './styles';
+import { Body, ContentLabel } from '@utils/type';
+import space, { H_PADDING_MOBILE } from '@utils/space';
+import { mq } from '@utils/styles';
+import { fonts } from '@utils/fonts';
+
+export const H2 = styled.h2`
+  font-family: ${fonts.GRANVILLE};
+  font-size: 1.2rem;
+  margin-bottom: ${space[3]};
+`;
+
+export const HelpText = styled.div`
+  width: 100%;
+  margin-top: ${space[4]};
+  margin-bottom: ${space[6]};
+
+  ${mq.gtlg} {
+    width: 50%;
+    margin-right: ${space[8]};
+    margin-bottom: 0;
+  }
+`;
+
+export const TalkWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: ${space[8]};
+  margin-bottom: ${space[8]};
+  padding: 0 ${H_PADDING_MOBILE};
+
+  ${mq.gtlg} {
+    align-items: center;
+    margin-top: 0;
+    padding: ${space[10]} 0 ${space[4]};
+    text-align: center;
+  }
+`;
+
+export const TextWrapper = styled.div`
+  max-width: ${space[14]};
+  margin-bottom: ${space[7]};
+`;
+
+export const Emails = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  ${mq.gtlg} {
+    flex-direction: row;
+    justify-content: center;
+    text-align: left;
+  }
+`;
+
+export const Email = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-bottom: ${space[4]};
+  text-align: center;
+
+  ${mq.gtlg} {
+    padding: 0 ${space[6]};
+    text-align: left;
+
+    &:not(:last-child) {
+      border-right: 1px solid #333;
+    }
+  }
+`;
 
 const Contact = () => {
-  // const [name, setName] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [orderNumber, setOrderNumber] = useState('');
-  // const [message, setMessage] = useState('');
-
   return (
     <>
-      {/* <styled.Container>
-        <styled.HelpText>
-          <styled.H200>I need help with an order!</styled.H200>
-          <Body>
-            Questions about your order? Please do not hesitate to use our yellow
-            floating help widget to get in touch with a real life human being
-            from SANDALBOYZ. We will respond to any and every topic regarding
-            your order within 24 hours. Guaranteed to be robot free.
-          </Body>
-        </styled.HelpText>
-        <styled.Form data-netlify="true" name="Contact Form" method="POST">
-          <styled.H400>Human Contact Help Form</styled.H400>
-          <styled.FormGroup>
-            <Input
-              label="Name"
-              name="name"
-              placeholder="Full name"
-              value={name}
-              onChange={evt => {
-                setName(evt.target.value);
-              }}
-            />
-            <Input
-              label="Email Address"
-              name="email"
-              placeholder="email@example.com"
-              type="email"
-              value={email}
-              onChange={evt => {
-                setEmail(evt.target.value);
-              }}
-            />
-            <Input
-              label="Order Number (If Applicable)"
-              name="orderNumber"
-              placeholder="ABCDE123"
-              value={orderNumber}
-              onChange={evt => {
-                setOrderNumber(evt.target.value);
-              }}
-            />
-          </styled.FormGroup>
-          <Textarea
-            label="Message"
-            name="message"
-            value={message}
-            onChange={evt => {
-              setMessage(evt.target.value);
-            }}
-          />
-          <input type="hidden" name="form-name" value="Contact Form" />
-          <styled.FormActions>
-            <Button size="small" type="submit">
-              Send message
-            </Button>
-          </styled.FormActions>
-        </styled.Form>
-      </styled.Container> */}
-
-      <styled.TalkWrapper>
-        <styled.TextWrapper>
-          <styled.H200>I need help with an order!</styled.H200>
+      <TalkWrapper>
+        <TextWrapper>
+          <H2>I need help with an order!</H2>
           <Body>
             Questions about your order? Please do not hesitate to use our yellow
             floating help widget to get in touch with a real life human being
@@ -89,9 +91,9 @@ const Contact = () => {
             </span>
             .
           </Body>
-        </styled.TextWrapper>
-        <styled.TextWrapper>
-          <styled.H200>I just wanna talk!</styled.H200>
+        </TextWrapper>
+        <TextWrapper>
+          <H2>I just wanna talk!</H2>
           <Body>
             Have a non-order related question? Press? Sales? Just need someone
             to talk to? Shoulder to lean on? Please direct all other inquiries
@@ -99,16 +101,16 @@ const Contact = () => {
             email address will (unfortunately) be laughed at and not responded
             to. Just kidding, we respond to everything.
           </Body>
-        </styled.TextWrapper>
-        <styled.Emails>
-          <styled.Email>
-            <styled.H600>Let's fucking talk:</styled.H600>
+        </TextWrapper>
+        <Emails>
+          <Email>
+            <ContentLabel>Let's fucking talk:</ContentLabel>
             <a href="mailto:contact@sandalboyz.com">
               <Body>contact@sandalboyz.com</Body>
             </a>
-          </styled.Email>
-        </styled.Emails>
-      </styled.TalkWrapper>
+          </Email>
+        </Emails>
+      </TalkWrapper>
     </>
   );
 };
--- src/components/ContactPage/styles.js
+++ src/components/ContactPage/styles.js
@@ -1,125 +1,13 @@
 import styled from 'styled-components';
 
-import colors from '@utils/colors';
 import space, { H_PADDING_MOBILE } from '@utils/space';
-import { Container as BaseContainer, mq } from '@utils/styles';
-import * as type from '@utils/type';
+import { mq } from '@utils/styles';
+import { fonts } from '@utils/fonts';
 
-export const Container = styled(BaseContainer)`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  margin-top: 48px;
-  padding-top: ${space[4]} !important;
-
-  ${mq.gtlg} {
-    flex-direction: row;
-    margin-top: 80px;
-    padding-top: ${space[7]} !important;
-  }
-`;
-
-export const Emails = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  ${mq.gtlg} {
-    flex-direction: row;
-    justify-content: center;
-    text-align: left;
-  }
-`;
-
-export const Email = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  margin-bottom: ${space[4]};
-  text-align: center;
-
-  ${mq.gtlg} {
-    padding: 0 ${space[6]};
-    text-align: left;
-
-    &:not(:last-child) {
-      border-right: 1px solid ${colors.N200};
-    }
-  }
-`;
-
-export const Form = styled.form`
-  width: 100%;
-  padding: ${space[4]};
-  background-color: ${colors.N100};
-
-  ${mq.gtlg} {
-    width: 50%;
-    background: transparent;
-    border: 1px solid ${colors.N200};
-  }
-`;
-
-export const FormActions = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-end;
-  margin-top: ${space[3]};
-
-  & > :first-child {
-    margin-bottom: ${space[3]};
-  }
-
-  ${mq.gtlg} {
-    align-items: center;
-    flex-direction: row;
-
-    & > :first-child {
-      margin-bottom: 0;
-    }
-  }
-`;
-
-export const FormGroup = styled.div`
-  display: flex;
-  flex-direction: column;
+export const H2 = styled.h2`
+  font-family: ${fonts.GRANVILLE};
+  font-size: 1.2rem;
   margin-bottom: ${space[3]};
-
-  & > * {
-    flex: 1;
-    width: 100%;
-  }
-
-  & > *:not(:last-child) {
-    margin-bottom: ${space[3]};
-  }
-
-  ${mq.gtlg} {
-    flex-direction: row;
-
-    & > *:not(:last-child) {
-      margin-right: ${space[3]};
-      margin-bottom: 0;
-    }
-  }
-`;
-
-export const H200 = styled(type.H200)`
-  margin-bottom: ${space[3]};
-`;
-
-export const H300 = styled(type.H300)`
-  margin-bottom: ${space[4]};
-`;
-
-export const H400 = styled(type.H400)`
-  margin-bottom: ${space[3]};
-`;
-
-export const H600 = styled(type.H600)`
-  ${mq.gtlg} {
-    margin-bottom: ${space[2]};
-  }
 `;
 
 export const HelpText = styled.div`
