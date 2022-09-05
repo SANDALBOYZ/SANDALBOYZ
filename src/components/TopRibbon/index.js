185f42bdf1dac7e0620a54b70c856422ad43042b
Ryan Chang <rchang@videoamp.com>
2022-09-05 13:52:08 -0700

    top ribbon component

---
+++ src/components/TopRibbon/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const TopRibbon = () => {
+  return (
+    <div>
+      Labor Day Flash Sale - 24 hours only! Subscribe to our newsletter to get
+      access.
+    </div>
+  );
+};
+
+export default TopRibbon;
