a0011fe5cf5e6108822102a088467ce7e1a1004f
Ryan Chang <rchang@videoamp.com>
2022-09-05 14:15:56 -0700

    add ribbon

--- src/components/TopRibbon/index.js
+++ src/components/TopRibbon/index.js
@@ -1,12 +1,44 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+import { fonts, weights } from '@utils/fonts';
+
+const Ribbon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 8px 0;
+  background-color: white;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: 10000;
+`;
+
+const SubscribeClick = styled.span`
+  text-decoration: underline;
+  margin: 0 0.2em;
+  cursor: pointer;
+`
+
 const TopRibbon = () => {
+  const subscribeOnClick = () => {
+    window.scroll({
+      top: document.body.offsetHeight,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
+
   return (
-    <div>
-      Labor Day Flash Sale - 24 hours only! Subscribe to our newsletter to get
-      access.
-    </div>
+    <Ribbon>
+      Labor Day Flash Sale - 24 hours only for 25% off!{' '}
+      <SubscribeClick onClick={subscribeOnClick}>
+        Subscribe to our newsletter
+      </SubscribeClick>{' '}
+      to get access.
+    </Ribbon>
   );
 };
 
