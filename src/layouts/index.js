2d3d2e8abf481359ad9dda013ce9ccc545387856
Ryan Chang <rchang@videoamp.com>
2022-09-08 09:24:35 -0700

    revert ribon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -235,7 +235,7 @@ class Layout extends React.Component {
       <StoreContext.Provider value={this.state.store}>
         <GlobalStyle />
         <>
-          <TopRibbon />
+          {/* <TopRibbon /> */}
           <Navigation
             animate
             authLinks={this.getNavAuthLinks()}
